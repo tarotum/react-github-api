9e1582782fbb420fe6d42fdc2fc59c1fa0445f7f
tarotum <onyskiv.maxim@gmail.com>
2019-04-25 22:17:10 +0300

    refactor users

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -2,16 +2,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import User from './User';
 
-const Users = ({ users }) => (
-  <div>
-    {users.map(item => (
-      <User key={item.id} login={item.login} />
-    ))}
-  </div>
-);
+const Users = ({ users, isLoading, error }) => {
+  if (error !== '') {
+    return <p>{error}</p>;
+  }
+  if (isLoading && !users.length) {
+    return <p>Loading...</p>;
+  }
+  if (users.length === 0) {
+    return <p>Nothing found</p>;
+  }
+  return (
+    <div className="user-list">
+      {users.map(item => (
+        <User key={item.id} user={item} />
+      ))}
+    </div>
+  );
+};
 
 Users.propTypes = {
-  users: PropTypes.object.isRequired,
+  users: PropTypes.arrayOf(PropTypes.any).isRequired,
+  isLoading: PropTypes.bool.isRequired,
+  error: PropTypes.string.isRequired,
 };
 
 export default Users;
