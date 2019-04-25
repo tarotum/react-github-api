29b8c690dbdb623b2fed0b6f97c3ad823f18a8ff
tarotum <onyskiv.maxim@gmail.com>
2019-04-25 22:16:39 +0300

    refactor single user component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,19 +1,20 @@
+/* eslint-disable camelcase */
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const User = ({ login }) => (
-  <div>
-User -
-    {login}
+const User = ({ user }) => (
+  <div className="user">
+    <a href={user.html_url}>
+      <img width="50" height="50" src={user.avatar_url} alt={user.login} />
+    </a>
+    <a href={user.html_url}>
+      <h4>{user.login}</h4>
+    </a>
   </div>
 );
 
 User.propTypes = {
-  login: PropTypes.string,
-};
-
-User.defaultProps = {
-  login: 'User login',
+  user: PropTypes.objectOf(PropTypes.any).isRequired,
 };
 
 export default User;
