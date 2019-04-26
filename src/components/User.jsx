327bbfa8125d1995a3e3a89719ebcc98628f7542
tarotum <onyskiv.maxim@gmail.com>
2019-04-26 19:13:15 +0300

    add styles for User component

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,16 +1,40 @@
 /* eslint-disable camelcase */
 import React from 'react';
+import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+const UserWrapper = styled.a`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1em;
+  background-color: transparent;
+  transition: background-color 0.5s;
+  &:last-child {
+    margin-bottom: 0;
+  }
+  &:hover {
+    background-color: rgba(255, 165, 0, 0.5);
+  }
+`;
+
+const Image = styled.img`
+  display: flex;
+  margin-right: 1em;
+  widht: 3em;
+  height: 3em;
+`;
+
+const H4 = styled.h4`
+  margin: 0;
+  line-height: 1;
+  font-size: 1.2em;
+`;
+
 const User = ({ user }) => (
-  <div className="user">
-    <a href={user.html_url}>
-      <img width="50" height="50" src={user.avatar_url} alt={user.login} />
-    </a>
-    <a href={user.html_url}>
-      <h4>{user.login}</h4>
-    </a>
-  </div>
+  <UserWrapper href={user.html_url} title={user.login}>
+    <Image src={user.avatar_url} alt={user.login} />
+    <H4>{user.login}</H4>
+  </UserWrapper>
 );
 
 User.propTypes = {
