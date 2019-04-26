eb675fffbcbfba3db67a76fe1b0edb8d214244b9
tarotum <onyskiv.maxim@gmail.com>
2019-04-26 17:25:47 +0300

    add stysel for app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
-import Users from './containers/UsersContainer';
+import UsersContainer from './containers/UsersContainer';
 
-export default () => (
-  <div>
-    <Users />
-  </div>
+const AppWrapper = styled.div`
+  max-width: 970px;
+  width: 100%;
+  margin-right: auto;
+  margin-left: auto;
+  padding-right: 1em;
+  padding-left: 1em;
+`;
+
+const App = () => (
+  <AppWrapper>
+    <UsersContainer />
+  </AppWrapper>
 );
+
+export default App;
